d7a3ea801965f73b6a3af913296ad0fa84bbf6ef
Dev Jangid <devjangid788@gmail.com>
2026-05-23 20:44:36 +0530

    fix case sensitive imports

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import AuthContextProvider from './Context/authContext.jsx'
+import AuthContextProvider from './Context/AuthContext.jsx'
 import UserContext from './Context/UserContext.jsx'
 import ShopContext from './Context/ShopContext.jsx'
 import axios from "axios"
